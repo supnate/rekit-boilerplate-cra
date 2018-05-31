c12dd3675ace8c73749c05cdb1f3c552ad93956a
Nate Wang <supnate@gmail.com>
2018-05-31 22:56:39 +0800

    Typo fix.

--- src/common/rootReducer.js
+++ src/common/rootReducer.js
@@ -7,7 +7,7 @@ import examplesReducer from '../features/examples/redux/reducer';
 // NOTE 1: DO NOT CHANGE the 'reducerMap' name and the declaration pattern.
 // This is used for Rekit cmds to register new features, remove features, etc.
 // NOTE 2: always use the camel case of the feature folder name as the store branch name
-// So that it's easy for others to understand it and Rekit could manage theme.
+// So that it's easy for others to understand it and Rekit could manage them.
 
 const reducerMap = {
   router: routerReducer,
